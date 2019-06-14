d5c66b0d76c351fbe7409ff5eb1833ae48c2fcb1
AJ <ahumphries102@gmail.com>
2019-06-14 13:54:30 -0500

    Fetching data using async await

--- promiseslearning.js
+++ promiseslearning.js
@@ -1,48 +1,58 @@
-const bedMade = ()=>{
-  const bedmade = false
-  return new Promise(function(resolve, reject){
-    if(bedmade){
-    resolve('I made the bed ')
-    }
-    else{
-      reject('I did not make the bed')
-    }
-  })
+const fetch = require( 'node-fetch')
+
+async function getJunk(){  
+  try{
+    let junk = await fetch('https://api.scryfall.com/cards/search?order=set&q=e%3Arna&unique=prints')
+    .then(response => response.json())
+    .then(result => result.data.forEach( result => {console.log(result.name)}))
+  }
+  catch(err){
+    console.log(err)
+  }
 }
 
-const litterScooped = (message)=>{
-  const litterscooped = true
-  return new Promise(function(resolve,reject){
-    if(litterscooped){
-    resolve(message + 'The garbage is emptied')
-  }
-  else{
-    reject('Best start scoopin them litters boi')
-  }
-  })
-}
+getJunk()
+// const bedMade = ()=>{
+//   const bedmade = true
+//   return new Promise((resolve, reject)=>{
+//     setTimeout(()=>{
+//       if(bedmade){
+//         resolve('hooray bed is made ')
+//       }
+//     }, 400)
+//   })
+// }
 
-const catsPetted = (message)=>{
-  const choresDone = true
-  return new Promise(function(resolve,reject){
-    if(choresDone){
-    resolve(message + ' yummmmmy')
-  }
-  })
-}
+// const litterScooped = (message)=>{
+//   const litterscooped = true
+//   return new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//       if(litterscooped){
+//         resolve(message + 'The garbage is emptied')
+//       }
+//     },500)
+//   })
+// }
 
-bedMade()
-.then( value => {
-  return litterScooped(value)
-}, reason =>{
-  return litterScooped(reason)
-})
-.then( value => {
-  return catsPetted(value)
-})
-.then( value => {
-  return console.log(value)
-})
-.catch( error => {
-  console.error(error)
-})
+// const catsPetted = (message)=>{
+//   const choresDone = true
+//   return new Promise((resolve,reject)=>{
+//     if(choresDone){
+//     resolve(message + ' yummmmmy')
+//   }
+//   })
+// }
+
+// bedMade()
+// .then( value => {
+//   return litterScooped(value)
+// })
+// .then( value => {
+//   return catsPetted(value)
+// })
+// .then( value => {
+//   return console.log(value)
+// })
+// .catch( error => {
+//   console.error(error)
+// })
